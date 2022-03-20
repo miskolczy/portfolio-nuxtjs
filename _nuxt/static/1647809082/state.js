b190c05647365ab8732c8401adecd98704004765
miskolczy <miskolczy@users.noreply.github.com>
2022-03-20 20:44:44 +0000

    deploy: 2f0bec516fc85f6bce0a856a03a2e9ffc2fa8826

---
+++ _nuxt/static/1647809082/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio-nuxtjs\u002F_nuxt\u002Fstatic\u002F1647809082",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fportfolio-nuxtjs\u002F",assetsPath:"\u002Fportfolio-nuxtjs\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b77e0507"}}}}(null));
