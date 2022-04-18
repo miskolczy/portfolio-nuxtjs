1f0e5907e76c806a381c0b1143e966c3d43902bf
miskolczy <miskolczy@users.noreply.github.com>
2022-04-18 09:11:47 +0000

    deploy: 35b831d6708abbd9f8fb575c12428151d3429eb4

--- _nuxt/static/1650238889/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1650238889",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"945a1fb7"}}}}(null,"\u002F"));
